aa4d30e9b246e038dc8b774e3ff534e41f89913f
andrea29292 <andrea.ceccarelli29292@gmail.com>
2016-06-08 12:16:07 +0200

    implemented decision tree, non recursive

---
+++ Assets/Scripts/IA/CrowdIA.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrowdIA : MonoBehaviour {
+
+    DecisionTree CrowdTree;
+    // Use this for initialization
+    void Start() {
+        DTDecision d1 = new DTDecision(randomNumber);
+
+
+        CrowdTree = new DecisionTree(d1);
+        DTAction a1 = new DTAction(trueOption);
+        DTAction a2 = new DTAction(falseOption);
+        d1.AddNode(true, a1);
+        d1.AddNode(false, a2);
+        StartCoroutine(Patrol(CrowdTree));
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    object randomNumber() {
+        return Random.value > 0.5 ? true : false;
+    }
+
+    void trueOption() {
+        Debug.Log("Opzione true");
+    }
+
+    void falseOption() {
+        Debug.Log("Opzione false");
+    }
+
+    IEnumerator Patrol(DecisionTree tree) {
+        while (true) {
+            tree.Walk();
+            yield return new WaitForSeconds(2f);
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/IA/DecisionTree.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+
+
+public delegate void DTActionCall();
+public delegate object DTDecisionCall();
+
+public abstract class DTNode {
+
+}
+
+
+
+
+public class DTAction : DTNode {
+
+    DTActionCall action;
+
+    public DTAction(DTActionCall action) {
+        this.action = action;
+    }
+
+    public void ExecuteAction() {
+        action();
+    }
+}
+
+public class DTDecision : DTNode {
+    private DTDecisionCall Selector;
+    private Dictionary<object, DTNode> Arcs;
+
+    public DTDecision(DTDecisionCall selector) {
+        Selector = selector;
+        Arcs = new Dictionary<object, DTNode>();
+    }
+    public void AddNode(object arc, DTNode node) {
+        Arcs.Add(arc, node);
+    }
+    public void takeDecision(ref DTNode node) {
+        object decision = Selector();
+        node = Arcs.ContainsKey(decision) ? Arcs[decision] : null;
+    }
+}
+
+public class DecisionTree {
+    DTNode currentNode;
+    DTNode startNode;
+
+    public DecisionTree(DTNode startNode) {
+        this.startNode = startNode;
+    }
+
+    public void Walk() {
+        currentNode = startNode;
+
+        while (currentNode != null) {
+            if (currentNode is DTAction) {
+                //new DTAction(currentNode).ExecuteAction();
+                DTAction actionNode = currentNode as DTAction;
+                actionNode.ExecuteAction();
+                Debug.Log("Azione eseguita");
+                currentNode = null;
+                return;
+            }
+            else if (currentNode is DTDecision) {
+                DTDecision decisionNode = currentNode as DTDecision;
+                decisionNode.takeDecision(ref currentNode);
+            }
+        }
+        Debug.Log("Nessuna azione trovata");
+    }
+
+}
+
+
